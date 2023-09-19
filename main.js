30ec0d75addbf8cb9c1a016c103d517c026aff03
malenabrun <135130796+malenabrun@users.noreply.github.com>
2023-09-19 20:37:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+/* https://barba.js.org/ */
+window.addEventListener("load", () => {
+    barba.init({
+        transitions: [{
+            name: "opacity-transition",
+            leave(data) {
+                return gsap.to(data.current.container, {
+                    opacity: 0,
+                })
+            },
+            enter(data) {
+                return gsap.from(data.next.container, {
+                    opacity: 0,
+                })
+            },
+        }, ],
+    })
+})
